8e4ab619aeb8681ff27cea7eb64fbce61c4ee06b
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-08-25 17:50:52 +0530

    main

--- frontend/src/Components/RentNow.jsx
+++ frontend/src/Components/RentNow.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../CSS/BuyNow.css';
 import radio from '../Images/radio.jpg';
 
-function ProductInfo(){
-   
-       return(
-        <div className="d-flex">
-        <div className="container bg-white">
+function ProductInfo() {
+
+  return (
+    <div className="d-flex">
+      <div className="container bg-white">
         <div className="row">
           <div className="col-md-8">
             <img
@@ -14,40 +14,40 @@ function ProductInfo(){
               alt="radio"
               className="img-fluid"
               style={{ margin: "1.2rem 0px 0px 6rem " }}
-              
+
             ></img>
-            </div>
-            <div>
-            <h2 className="text-dark"><b><u>Radio</u></b></h2>
-            </div>
-            <div className="data">
-              <h6><b>Brand</b>    : Carvaan</h6>
-              <h6><b>Colour</b>   : Black</h6>
-              <h6><b>Special Feature</b>  :  Portable</h6>
-              <h6><b>Connectivity Technology</b>  :  Bluetooth</h6>
-              <h6><b>Product Dimensions</b>  :  21L x 24W x 7.8H Centimeters</h6>
-            </div>
-            <div className="Des fs-7 text-dark">
-            <p><b>Saregama Carvaan Lite Hindi - Portable Music Player with 3000 Pre-Loaded Evergreen Songs, FM/BT/AUX</b></p>
-            </div>
-            </div>
           </div>
-                <div className="Detail ">
-                   
-                    
-                    <div className="price">
-                    <h5 className="text-danger">Rs. 4,990</h5>
-                    <h6><br/>Inclusive of all taxes
-                    EMI starts at ₹242. No Cost EMI available.</h6>
-                    </div>
-                    <button><b>BuyNow</b></button>
-                </div>
-            
-        
-        
+          <div>
+            <h2 className="text-dark"><b><u>Radio</u></b></h2>
+          </div>
+          <div className="data">
+            <h6><b>Brand</b>    : Carvaan</h6>
+            <h6><b>Colour</b>   : Black</h6>
+            <h6><b>Special Feature</b>  :  Portable</h6>
+            <h6><b>Connectivity Technology</b>  :  Bluetooth</h6>
+            <h6><b>Product Dimensions</b>  :  21L x 24W x 7.8H Centimeters</h6>
+          </div>
+          <div className="Des fs-7 text-dark">
+            <p><b>Saregama Carvaan Lite Hindi - Portable Music Player with 3000 Pre-Loaded Evergreen Songs, FM/BT/AUX</b></p>
+          </div>
         </div>
-       )
-       
+      </div>
+      <div className="Detail ">
+
+
+        <div className="price">
+          <h5 className="text-danger">Rs. 4,990</h5>
+          <h6><br />Inclusive of all taxes
+            EMI starts at ₹242. No Cost EMI available.</h6>
+        </div>
+        <button><b>BuyNow</b></button>
+      </div>
+
+
+
+    </div>
+  )
+
 
 }
 
--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -10,7 +10,7 @@ function Login() {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    const[contact, setContact]=useState('');
+    const [contact, setContact] = useState('');
     const [error, setError] = useState('');
     const handleChange = (e) => {
         setFormData({
@@ -37,53 +37,53 @@ function Login() {
                 <form onSubmit={handleSubmit}>
                     <h2 className="mb-4 fs-5"><b><u>Signup for new account</u></b></h2>
                     <div className="form-group">
-                        <label><FaUser className="icon"/><b>UserName:</b></label>
+                        <label><FaUser className="icon" /><b>UserName:</b></label>
 
 
 
 
                         <input type="username" id="username" placeholder="username" onChange={handleChange} required />
-                        
+
                     </div>
                     <div className="form-group">
-                        <label><FaLock className="icon"/><b>Password</b></label>
+                        <label><FaLock className="icon" /><b>Password</b></label>
 
 
 
 
                         <input type="password" id="password" placeholder="password" onChange={handleChange} required />
-                        
+
                     </div>
-                    
-                    
-                    
+
+
+
                     <div className="form-group">
-                        <label><MdEmail className="icon"/><b>Email:</b></label>
+                        <label><MdEmail className="icon" /><b>Email:</b></label>
 
 
 
 
                         <input type="email" id="password" placeholder="email" onChange={handleChange} required />
-                        
+
                     </div>
                     <div className="form-group">
-                        <label><IoMdContacts className="icon"/><b>Contacts:</b></label>
+                        <label><IoMdContacts className="icon" /><b>Contacts:</b></label>
 
 
 
 
                         <input type="contacts" id="contacts" placeholder="contacts" onChange={handleChange} required />
-                        
+
                     </div>
-                     
-                     
-                    
+
+
+
 
                     <div className="form-group mt-2">
                         <button type="submit">SignIn</button>
                     </div>
                     <h4 className="fs-6"><b>Already have an account? Login</b></h4>
-                    
+
 
                 </form>
             </div>
