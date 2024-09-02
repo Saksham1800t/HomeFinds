0cac09533664f4ca8d98fe627f0cebac69b52d97
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-09-02 13:58:39 +0530

    saksham

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Components/LandingPages/Home';
 import BuyNow from './Components/BuyNow';
-import ProductAdd  from './Components/Product/ProductAdd'; 
-import Login from './Components/User/Login'; 
+import ProductAdd from './Components/Product/ProductAdd';
+import Login from './Components/User/Login';
+import AdminLogin from './Components/Admin/AdminLogin';
+import AdminDashboard from './Components/Admin/Dashboard';
+import BuyPage from './Components/BuyPage';
+import RentPage from './Components/RentPage';
 
 function App() {
   const router = createBrowserRouter([
@@ -21,12 +25,28 @@ function App() {
     {
       path: '/login',
       element: <Login />
+    },
+    {
+      path: '/AdminLogin',
+      element: <AdminLogin />
+    },
+    {
+      path: '/AdminDashboard',
+      element: <AdminDashboard />
+    },
+    {
+      path: '/BuyPage',
+      element: <BuyPage />
+    },
+    {
+      path: '/RentPage',
+      element: <RentPage />
     }
   ])
   return (
-   <>
-   <RouterProvider router={router}/>
-   </>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
